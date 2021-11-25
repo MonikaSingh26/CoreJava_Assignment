312479c6c78e4bee928bb449308e1b305d0c9fd8
Monika Singh <monikasingh0126@gmail.com>
2021-11-25 17:25:04 +0530

    Fixed code identation

--- src/Assignment2/Cylinder.java
+++ src/Assignment2/Cylinder.java
@@ -2,34 +2,32 @@ package Assignment2;
 
 public class Cylinder {
 
-	   public double lateralSurfaceArea(double radius, double height) {
-			try {
-	            return 2 * Math.PI * radius * height; //Return Lateral Surface Area of the Cylinder
-	        }
-	        catch (ArithmeticException ae) {
-	        	ae.getMessage();
-	            return (0.0);
-	        }
-	    }
-		public double surfaceArea(double radius, double height) {
-
-	        try {
-	            return (2 * Math.PI * radius * height) + (2 * Math.PI * radius * 2); //Return Surface Area of the Cylinder
-	        }
-	        catch (ArithmeticException ae) {
-	        	ae.getMessage();
-	            return (0.0);
-	        }
+	public double lateralSurfaceArea(double radius, double height) {
+		try {
+			return 2 * Math.PI * radius * height; // Return Lateral Surface Area of the Cylinder
+		} catch (ArithmeticException ae) {
+			ae.getMessage();
+			return (0.0);
 		}
-	     public double volume(double radius, double height) {
-
-	            try {
-	                return Math.PI * Math.pow(radius, 2) * height; //Return Volume of the Cylinder
-	            }
-	            catch (ArithmeticException ae) {
-	            	ae.getMessage();
-	            	return (0.0);
-	            }
-	    }
 	}
 
+	public double surfaceArea(double radius, double height) {
+
+		try {
+			return (2 * Math.PI * radius * height) + (2 * Math.PI * radius * 2); // Return Surface Area of the Cylinder
+		} catch (ArithmeticException ae) {
+			ae.getMessage();
+			return (0.0);
+		}
+	}
+
+	public double volume(double radius, double height) {
+
+		try {
+			return Math.PI * Math.pow(radius, 2) * height; // Return Volume of the Cylinder
+		} catch (ArithmeticException ae) {
+			ae.getMessage();
+			return (0.0);
+		}
+	}
+}
--- src/Assignment2/Square.java
+++ src/Assignment2/Square.java
@@ -1,32 +1,31 @@
 package Assignment2;
 
 public class Square {
-	
+
 	public double perimeter(double side) {
 		try {
-            return 4 * (side); //Return Perimeter of the Square
-        }
-        catch (ArithmeticException ae) {
-        	ae.getMessage();
-            return (0.0);
-        }
-    }
+			return 4 * (side); // Return Perimeter of the Square
+		} catch (ArithmeticException ae) {
+			ae.getMessage();
+			return (0.0);
+		}
+	}
+
 	public double area(double side) {
 		try {
-            return (side * side); //Return Area of the Square
-        }
-        catch (ArithmeticException ae) {
-        	ae.getMessage();
-            return (0.0);
-        }
+			return (side * side); // Return Area of the Square
+		} catch (ArithmeticException ae) {
+			ae.getMessage();
+			return (0.0);
+		}
 	}
+
 	public double diagonal(double side) {
 		try {
-            return Math.pow(side, 2); //Return Diagonal of the Square
-        }
-        catch (ArithmeticException ae) {
-        	ae.getMessage();
-            return (0.0);
-        }
+			return Math.pow(side, 2); // Return Diagonal of the Square
+		} catch (ArithmeticException ae) {
+			ae.getMessage();
+			return (0.0);
+		}
 	}
 }
